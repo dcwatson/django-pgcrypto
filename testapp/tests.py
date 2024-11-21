f1404e7ba13d6bf61dea6463c52a17a73b4fd3fa
ruttenb <ruttenb@imsweb.com>
2024-11-21 10:45:36 -0500

    Improt Decrypt

--- testapp/tests.py
+++ testapp/tests.py
@@ -14,6 +14,7 @@ from django.test import TestCase
 
 from pgcrypto import __version__, armor, dearmor, pad, unpad
 from pgcrypto.fields import BaseEncryptedField
+from pgcrypto.functions import Decrypt
 
 from .models import Employee
 from django.db.models import Value
